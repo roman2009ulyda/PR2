4d80b5dad49419f3554e2cdb63660120e8668338
roma <roman2009ulyda@gmail.com>
2026-09-18 00:47:43 +0300

    1 commit

---
+++ PR2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double x;
+	double y;
+	double z1;
+	double z2;
+	cout << "x=";
+	cin >> x;
+	cout << "y=";
+	cin >> y;
+	z1 = pow(cos(x), 4) + pow(sin(y), 2) + (1.0 / 4) * pow(sin(2 * x), 2) - 1;
+	z2 = sin(x + y) * sin(y - x);
+	cout << endl;
+	cout << "z1=" << z1 << endl;
+	cout << "z2=" << z2 << endl;
+}
