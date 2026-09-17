6e8e221b800fe90cf58495f8e957f5df454dfd41
roma <roman2009ulyda@gmail.com>
2026-09-18 00:56:25 +0300

    2 commit

--- PR2.cpp
+++ PR2.cpp
@@ -12,6 +12,6 @@ int main() {
 	z1 = pow(cos(x), 4) + pow(sin(y), 2) + (1.0 / 4) * pow(sin(2 * x), 2) - 1;
 	z2 = sin(x + y) * sin(y - x);
 	cout << endl;
-	cout << "z1=" << z1 << endl;
-	cout << "z2=" << z2 << endl;
+	//cout << "z1=" << z1 << endl;
+	//cout << "z2=" << z2 << endl;
 }
